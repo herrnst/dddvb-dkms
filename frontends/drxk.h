5575ddc9d3c509519d9cc6ae40fc040cb2b8cc0f
Daniel Scheller <d.scheller@gmx.net>
2014-04-19 15:00:27 +0200

    Remove ifdef's from drxk.h to fix name conflicts, also there's no reason to not build drxk.ko from this tree

--- frontends/drxk.h
+++ frontends/drxk.h
@@ -41,17 +41,7 @@ struct drxk_config {
 	const char *microcode_name;
 };
 
-#if defined(CONFIG_DVB_DRXK) || (defined(CONFIG_DVB_DRXK_MODULE) \
-        && defined(MODULE))
 extern struct dvb_frontend *drxk_attach(const struct drxk_config *config,
 					struct i2c_adapter *i2c);
-#else
-static inline struct dvb_frontend *drxk_attach(const struct drxk_config *config,
-					struct i2c_adapter *i2c)
-{
-        printk(KERN_WARNING "%s: driver disabled by Kconfig\n", __func__);
-        return NULL;
-}
-#endif
 
 #endif
