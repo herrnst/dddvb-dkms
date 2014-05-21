42ee7a02ed11cb766b73a2a2bd0bfbc201f2c1a2
Daniel Scheller <d.scheller@gmx.net>
2014-05-22 00:13:29 +0200

    put correct version number into global version define

--- ddbridge/ddbridge.h
+++ ddbridge/ddbridge.h
@@ -494,6 +494,6 @@ void ddbridge_mod_rate_handler(unsigned long data);
 
 int ddbridge_flashread(struct ddb *dev, u8 *buf, u32 addr, u32 len);
 
-#define DDBRIDGE_VERSION "0.9.15"
+#define DDBRIDGE_VERSION "0.9.15a"
 
 #endif
